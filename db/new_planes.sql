a083c8f5457b8878649c59a59338e125ec150803
SheaClose <SheaClose@gmail.com>
2018-06-14 12:33:18 -0500

    done

--- db/new_planes.sql
+++ db/new_planes.sql
@@ -1,3 +1,11 @@
+drop table if exists airplanes;
+
+CREATE TABLE airplanes (
+  plane_id SERIAL PRIMARY KEY NOT NULL,
+  plane_type varchar(40) NOT NULL,
+  passenger_count integer NOT NULL
+);
+
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('747', 200);
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('717', 10);
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('727', 20);
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const massive = require("massive");
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
+app.use(bodyParser.json());
+app.use(cors());
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  dbInstance.new_planes();
+  app.set("db", dbInstance);
+});
 
+app.get("/api/planes", (req, res) => {
+  let db = req.app.get("db");
+  db.get_planes().then(planes => {
+    return res.status(200).json(planes);
+  });
+});
+
+app.post("/api/planes", (req, res) => {
+  let db = req.app.get("db");
+  db.query(
+    "insert into airplanes (plane_type,passenger_count) values ($1, $2); select * from airplanes",
+    [req.body.plane_type, req.body.passenger_count]
+  ).then(planes => res.status(200).json(planes));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
